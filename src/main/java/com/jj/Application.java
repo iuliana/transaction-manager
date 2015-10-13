4de63a8abd65c58656f64cb6668c2e2c9c28f2c6
Iuliana Cosmina <iuliana.cosmina@gmail.com>
2015-10-14 01:26:25 +0300

    third commit

--- src/main/java/com/jj/Application.java
+++ src/main/java/com/jj/Application.java
@@ -1,8 +1,18 @@
 package com.jj;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * Created by iuliana.cosmina on 10/13/15.
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.jj.*"})
 public class Application {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
+
 }
